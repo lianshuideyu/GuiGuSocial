486a93c03b8b1a2804e1153497bf1ec8a0f436cf
lianshuideyu <834893479@qq.com>
2017-07-01 13:17:16 +0800

    Initial commit

---
+++ app/src/main/java/com/atguigu/guigusocial/common/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.atguigu.guigusocial.common;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/7/1.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+
+
+}
---
+++ app/src/main/java/com/atguigu/guigusocial/common/MyApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.guigusocial.common;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/7/1.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
