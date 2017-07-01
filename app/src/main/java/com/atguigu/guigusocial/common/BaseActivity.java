c7603dd5aecc738c7293c1849fa09712b153f0e3
lianshuideyu <834893479@qq.com>
2017-07-01 13:36:27 +0800

    初始化环信相关设置，抽取BaseActivity

--- app/src/main/java/com/atguigu/guigusocial/common/BaseActivity.java
+++ app/src/main/java/com/atguigu/guigusocial/common/BaseActivity.java
@@ -1,13 +1,38 @@
 package com.atguigu.guigusocial.common;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Administrator on 2017/7/1.
  */
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(getLayoutId());
+
+        initView();
+
+        initData();
+
+        initListener();
+    }
+
+    public abstract void initListener();
+
+    public abstract void initData();
+
+    public abstract void initView();
+
+    /**
+     * 添加布局id
+     * @return
+     */
+    public abstract int getLayoutId() ;
 
 }
--- app/src/main/java/com/atguigu/guigusocial/common/MyApplication.java
+++ app/src/main/java/com/atguigu/guigusocial/common/MyApplication.java
@@ -2,6 +2,9 @@ package com.atguigu.guigusocial.common;
 
 import android.app.Application;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+
 /**
  * Created by Administrator on 2017/7/1.
  */
@@ -12,6 +15,14 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-
+        EMOptions options = new EMOptions();
+// 默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
+        //是否自动添加群组
+        options.setAutoAcceptGroupInvitation(false);
+//初始化
+        EMClient.getInstance().init(this, options);
+//在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(true);
     }
 }
