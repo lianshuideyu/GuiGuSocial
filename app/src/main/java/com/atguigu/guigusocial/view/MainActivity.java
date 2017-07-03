bddd446e3963fb4bad3a8a55a94caf4b50ef1214
lianshuideyu <834893479@qq.com>
2017-07-03 10:41:48 +0800

    主页面布局，splash页面优化

--- app/src/main/java/com/atguigu/guigusocial/view/MainActivity.java
+++ app/src/main/java/com/atguigu/guigusocial/view/MainActivity.java
@@ -1,16 +1,20 @@
 package com.atguigu.guigusocial.view;
 
-import android.widget.TextView;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 
 import com.atguigu.guigusocial.R;
-import com.atguigu.guigusocial.bean.UserInfo;
 import com.atguigu.guigusocial.common.BaseActivity;
-import com.atguigu.guigusocial.model.Model;
-import com.atguigu.guigusocial.model.dao.AccountDAO;
+
+import butterknife.InjectView;
 
 public class MainActivity extends BaseActivity {
 
-    private TextView tvtest;
+
+    @InjectView(R.id.fragment_main)
+    FrameLayout fragmentMain;
+    @InjectView(R.id.tab_group)
+    RadioGroup tabGroup;
 
     @Override
     public void initListener() {
@@ -20,14 +24,14 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initData() {
 
-        tvtest = (TextView)findViewById(R.id.tvtest);
-
-        AccountDAO accountDAO = Model.getInstance().getAccountDAO();
-        if(accountDAO != null) {
-            UserInfo user = accountDAO.getUserInfo("zjc");
-
-            tvtest.setText("欢迎你： " + user.getName());
-        }
+//        tvtest = (TextView)findViewById(R.id.tvtest);
+//
+//        AccountDAO accountDAO = Model.getInstance().getAccountDAO();
+//        if(accountDAO != null) {
+//            UserInfo user = accountDAO.getUserInfo("zjc");
+//
+//            tvtest.setText("欢迎你： " + user.getName());
+//        }
     }
 
     @Override
---
+++ app/src/main/java/com/atguigu/guigusocial/view/fragment/BaseFragment.java
@@ -0,0 +1,8 @@
+package com.atguigu.guigusocial.view.fragment;
+
+/**
+ * Created by Administrator on 2017/7/2.
+ */
+
+public class BaseFragment {
+}
