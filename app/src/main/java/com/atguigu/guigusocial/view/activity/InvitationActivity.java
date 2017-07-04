13c94cd569aedad397b750cd9b545caf7c3aa687
lianshuideyu <834893479@qq.com>
2017-07-04 19:44:34 +0800

    接受和拒绝邀请

--- app/src/main/java/com/atguigu/guigusocial/view/activity/InvitationActivity.java
+++ app/src/main/java/com/atguigu/guigusocial/view/activity/InvitationActivity.java
@@ -6,7 +6,10 @@ import com.atguigu.guigusocial.R;
 import com.atguigu.guigusocial.bean.InvitationInfo;
 import com.atguigu.guigusocial.common.BaseActivity;
 import com.atguigu.guigusocial.model.Model;
+import com.atguigu.guigusocial.utils.UIUtils;
 import com.atguigu.guigusocial.view.adapter.InvitatedAdapter;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.exceptions.HyphenateException;
 
 import java.util.List;
 
@@ -42,20 +45,97 @@ public class InvitationActivity extends BaseActivity {
     private InvitatedAdapter.OnInvitateListener myInvitate
             = new InvitatedAdapter.OnInvitateListener() {
         @Override
-        public void inviteAccept(InvitationInfo info) {
-            showToast("接受");
+        public void inviteAccept(final InvitationInfo info) {
+//            showToast("接受");
+            //同意好友请求
+            //在子线程中进行
+            Model.getInstance().getGlobalThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    if (info != null) {
+
+                        try {//联网同意...
+
+                            String name = info.getUserInfo().getName();
+                            EMClient.getInstance().contactManager().acceptInvitation(name);
+//                            UIUtils.showToast("同意请求");
+                            //保存联系人
+//                            Model.getInstance().getHelperManager()
+//                                    .getContactsDAO()
+//                                    .saveContact(new UserInfo(name, name), true);
+                            //改变消息的状态
+                            Model.getInstance().getHelperManager()
+                                    .getInvitationDAO()
+                                    .updateInvitationStatus
+                                            (InvitationInfo.InvitationStatus.INVITE_ACCEPT, name);
+
+                            //刷新适配器
+                            UIUtils.UIThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    UIUtils.showToast("添加成功");
+                                    refreshData();
+
+                                }
+                            });
+
+                        } catch (HyphenateException e) {
+                            e.printStackTrace();
+                            UIUtils.showToast(e.getMessage());
+                        }
+                    }
+                }
+            });
+
         }
 
         @Override
-        public void inviteReject(InvitationInfo info) {
-            showToast("拒绝");
+        public void inviteReject(final InvitationInfo info) {
+//            showToast("拒绝");
+            //拒绝好友请求
+            //在子线程中进行
+            Model.getInstance().getGlobalThread().execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    if (info != null) {
+                        try {
+                            String name = info.getUserInfo().getName();
+                            EMClient.getInstance().contactManager().declineInvitation(name);
+
+
+                            //改变消息的状态
+                            //拒绝后移除消息
+                            Model.getInstance().getHelperManager()
+                                    .getInvitationDAO()
+                                    .removeInvitation(name);
+
+                            //刷新适配器
+                            UIUtils.UIThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    UIUtils.showToast("拒绝成功");
+                                    refreshData();
+
+                                }
+                            });
+                        } catch (HyphenateException e) {
+                            e.printStackTrace();
+                            UIUtils.showToast(e.getMessage());
+                        }
+                    }
+
+
+                }
+            });
+
+
         }
     };
 
 
     /*
     界面数据展示需要考虑三大数据源
-
     * 网络
     * 本地
     * 内存和页面
@@ -65,7 +145,7 @@ public class InvitationActivity extends BaseActivity {
         List<InvitationInfo> invitations =
                 Model.getInstance().getHelperManager().getInvitationDAO().getInvitations();
 
-        if(invitations != null) {
+        if (invitations != null) {
 
             adapter.refresh(invitations);
         }
